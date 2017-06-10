4f9ac93571ad3654b84c9a0ac6edf9d5c3d07592
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-06-10 11:59:33 -0400

    Add scope validation

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,7 @@
 class Match < ApplicationRecord
+  validates :user_1_id, :presence => true, :uniqueness => { :scope => :user_2_id }
+  validates :user_2_id, :presence => true, :uniqueness => { :scope => :user_1_id }
+
   belongs_to :user1, class_name: "User", foreign_key: "user_1_id"
   belongs_to :user2, class_name: "User", foreign_key: "user_2_id"
   has_many :messages
