8fabe58bcf8c0976f4f82b6e1c43028ff961cb3a
projectmaking123 <jason.89.wang@gmail.com>
2017-06-11 14:10:41 -0400

    mod User model to have conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,8 +2,9 @@ class ConversationsController < ApplicationController
 before_action :authenticate_user
 
   def index
-  @users = User.all
-  @conversations = Conversation.all
+    @user = current_user
+    @users = User.all
+    @conversations = Conversation.all
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_many :user_matches
   has_secure_password
 
-  has_many :conversations, :foreign_key => :sender_id , class_name: 'User'
-  has_many :conversations, :foreign_key => :recipient_id, class_name: 'User'
+  has_many :conversations, :foreign_key => :sender_id
+  has_many :conversations, :foreign_key => :recipient_id
 
   def full_name
     "#{self.first_name} #{self.last_name}"
