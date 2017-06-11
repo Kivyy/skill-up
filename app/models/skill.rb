ea418c9d16f575631ff15dbeb9e61ad275e3828a
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-06-11 18:05:21 -0400

    Add pg_search gem and methods

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -5,7 +5,20 @@ class Skill < ApplicationRecord
   belongs_to :subcategory
   belongs_to :user
 
-  def self.search(search)
-    where("LOWER(description) LIKE ? OR LOWER(title) LIKE ?","%#{search.downcase}%", "%#{search.downcase}%")
+  include PgSearch
+  # multisearchable :against => [:description]
+  pg_search_scope :search, against: [:description]
+
+  # def self.search(search)
+  #   where("LOWER(description) LIKE ? OR LOWER(title) LIKE ?","%#{search.downcase}%", "%#{search.downcase}%")
+  # end
+  
+  def self.description_search(query)
+    if query.present?
+      search(query)
+    else
+      scope
+    end
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610182218) do
+ActiveRecord::Schema.define(version: 20170611194513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,10 @@ ActiveRecord::Schema.define(version: 20170610182218) do
   end
 
   create_table "conversations", force: :cascade do |t|
-    t.bigint "sender_id"
-    t.bigint "recipient_id"
+    t.integer "sender_id"
+    t.integer "recipient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
-    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -39,6 +37,15 @@ ActiveRecord::Schema.define(version: 20170610182218) do
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "skills", force: :cascade do |t|
