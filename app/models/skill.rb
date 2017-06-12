b5b93a6f845ea70963e3e1c7fe403b5117e9fa25
projectmaking123 <jason.89.wang@gmail.com>
2017-06-12 17:51:21 -0400

    merging

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ApplicationRecord
   validates :title, :presence => true
-  validates :category, :presence => true, inclusion: { in: %w( Sports, Music, Cooking, Home, Education, Programming, Fitness) }
+  validates :category, :presence => true, inclusion: { in: %w( Sports Music Cooking Home Education Programming Fitness) }
 
   has_many :posts
 end
