16dbeed10ef1d1ad4090c6c97a3ce60fd66b5bbe
projectmaking123 <jason.89.wang@gmail.com>
2017-06-12 15:41:56 -0400

    mod model for post skill, skill index/show, messge migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :description, :presence => true
 
-  has_many :apprenticeship
+  has_many :apprenticeships
   belongs_to :skill
   belongs_to :creator, class_name: "User"
 end
