98259f80ee9292d3309dc10684e6c64de3fec724
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-06-12 19:48:52 -0400

    Successfull search of post descriptions, basic post index page to display post search results

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,18 @@
 class Post < ApplicationRecord
   validates :description, :presence => true
-  
+
   has_many :apprenticeships
   belongs_to :skill
   belongs_to :creator, class_name: "User"
+
+  include PgSearch
+  pg_search_scope :search, :against => :description
+
+  def self.description_search(query)
+    if query.present?
+      search(query)
+    else
+      scoped
+    end
+  end
 end
